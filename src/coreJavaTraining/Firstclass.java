14bb22584edaab0076717df5933e5d16e98fa7a0
Surya <surya.attili@gmail.com>
2021-11-01 00:44:54 +0530

    My first commit

---
+++ src/coreJavaTraining/Firstclass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package coreJavaTraining;
+
+/**
+ * @author surya
+ *
+ */
+public class Firstclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow Surya");
+		
+
+	}
+
+}
